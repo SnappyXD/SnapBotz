5aa0629fdd5d0dd5caa0471382906cfb52c5bc3d
DianXD <snappyisreal@gmail.com>
2024-12-09 08:40:35 +0700

    first commit

---
+++ all/security/adiwConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'approval': {
+    'num': '628813100198',
+    'text': "oke",
+    'set': "👀 Harus mendapatkan persetujuan oleh creator script, Jika ingin memakai script ini",
+    'greet': "*Disetujui Oleh Creator, Silahkan Restart Panel Atau Run Ulang script* ☠️ "
+  },
+  'creatorScript': '628813100198',
+  'filePath': "./all/approval",
+  'checkFilePath': "../setting/osaragi.js",
+  'codeToDetect': 'main();'
+};
